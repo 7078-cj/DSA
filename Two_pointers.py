4a91793cf1d2922bbb14f66a05f1860de9733b35
Ceejay Santos <7078ceejay@gmail.com>
2026-05-03 13:12:37 +0800

    2_pointers

---
+++ Two_pointers.py
@@ -0,0 +1,24 @@
+
+
+def sortedSquares(nums):
+    left = 0
+    right = len(nums)-1
+    result = []
+    
+    while left <= right:
+        if abs(nums[left]) > abs(nums[right]):
+            result.append(nums[left]**2)
+            left+=1
+        else:
+            result.append(nums[right]**2)
+            right-=1
+    
+    result.reverse()
+            
+    return result
+
+if __name__ == "__main__":
+    nums = [-4, -1, 0, 3, 10]
+    
+    r = sortedSquares(nums)
+    print(r)
